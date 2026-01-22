040b65b94cf993440b72b6492bea8db106205094
Fabricio Lages <fabriciolagess@gmail.com>
2026-01-22 12:00:38 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,23 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+/// AAA - Preparar, Agir, Assertar
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-  await page.getByTestId('search-order-id').fill('VLO-WLO6NW')  
   
+  //Act
+  await page.getByTestId('search-order-id').fill('VLO-WLO6NW')   
   await page.getByTestId('search-order-button').click() 
-
+  
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-WLO6NW')
-  
+
   await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO') 
 })
