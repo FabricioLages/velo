7ea10bee18c5f996db511b29194bee2d9fdada6a
Fabricio Lages <fabriciolagess@gmail.com>
2026-01-26 19:55:22 -0300

    Alterações no Código para Desafio

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -11,13 +11,11 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-WLO6NW')   
-  await page.getByTestId('search-order-button').click() 
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-WLO6NW')   
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-WLO6NW')
-
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO') 
+  
+  await expect(page.locator('p.font-mono', { hasText: 'VLO-WLO6NW' })).toContainText('VLO-WLO6NW')
+  await expect(page.getByText('APROVADO', { exact: true })).toBeVisible()
 })
